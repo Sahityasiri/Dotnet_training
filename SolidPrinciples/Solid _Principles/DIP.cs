7a212899ebcb3bf82a119511ac4faf4bc084867a
Geetha-Naramsetti <geethanaramsetti1010@gmail.com>
2024-07-23 07:08:20 +0530

    Solid Principles uploaded

---
+++ SolidPrinciples/Solid _Principles/DIP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid__Principles
+{
+    internal class DIP
+    {
+        public interface ISwitchable
+        {
+            void TurnOn();
+            void TurnOff();
+        }
+
+
+        public class LightSwitch
+        {
+            private ISwitchable device;
+
+            public LightSwitch(ISwitchable device)
+            {
+                this.device = device;
+            }
+
+            public void Toggle()
+            {
+                if (device != null)
+                {
+                    if (device.IsOn)
+                        device.TurnOff();
+                    else
+                        device.TurnOn();
+                }
+            }
+        }
+
+        public class Light : ISwitchable
+        {
+            public bool IsOn { get; private set; }
+
+            public void TurnOn()
+            {
+                IsOn = true;
+            }
+
+            public void TurnOff()
+            {
+                IsOn = false;
+            }
+        }
+    }
+}
